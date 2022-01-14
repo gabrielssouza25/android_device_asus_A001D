8be50485cb01808cfe6eb6217f49ec0514010987
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-14 02:58:43 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -434,6 +434,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
+    
+    # Overlays evox
+DEVICE_PACKAGE_OVERLAYS += \
+	 $(DEVICE_PATH)/overlay \
+	 $(DEVICE_PATH)/overlay-evo
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
